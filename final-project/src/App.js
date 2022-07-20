8bae8c47e1ba3a872e160a39bdff4fb56efbb964
Mike <bottone@gmail.com>
2022-07-19 21:26:05 -0400

    broke it then fixed it

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -6,8 +6,6 @@ import './App.css'
 
 function App() {
   const [beers, setBeers] = useState([])
-  // const [maltIngredients, setMaltIngredients] = useState('')
-  // const [hopsIngredients, setHopsIngredients] = useState('')
   useEffect(() => {
     const getBeers = async () => {
       const response = await axios.get(
@@ -15,24 +13,17 @@ function App() {
       )
       setBeers(response.data)
       console.log(response)
-      // for (let i = 0; i < response.data.length; i++) {
-      //   for (let j = 0; j < response.data[i].ingredients.malt.length; j++) {
-      //     setMaltIngredients(response.data[i].ingredients.malt[j].name)
-      //   }
-      //   for (let k = 0; k < response.data[i].ingredients.hops.length; k++) {
-      //     setHopsIngredients(response.data[i].ingredients.hops[k].name)
-      //     console.log(hopsIngredients)
-      //   }
-      // }
     }
     getBeers()
   }, [])
 
   return (
-    <div className="cards">
-      {beers.map((beer) => (
-        <BeerCard beer={beer} key={beer.id} />
-      ))}
+    <div>
+      <div className="cards">
+        {beers.map((beer) => (
+          <BeerCard beer={beer} key={beer.id}></BeerCard>
+        ))}
+      </div>
     </div>
   )
 }
--- final-project/src/components/BeerCard.jsx
+++ final-project/src/components/BeerCard.jsx
@@ -8,17 +8,23 @@ const BeerCard = (props) => {
           <h1>{props.beer.name}</h1>
           <h2>{props.beer.tagline}</h2>
           <h3>ABV: {props.beer.abv}</h3>
-          <p className="desc">{props.beer.description}</p>
-          <h4>Hops:</h4>
+          <p className="desc"><em>{props.beer.description}</em></p>
+          <div className="bullets">
+          <div className="hops">
+          <h4>Hops</h4>
           <ul>{props.beer.ingredients.hops.map((hop) => (
           <li>{hop.name}</li>
           ))}</ul>
-          <h4>Malts:</h4>
+          </div>
+          <div className="malts">
+          <h4>Malts</h4>
           <ul>{props.beer.ingredients.malt.map((malt) => (
           <li>{malt.name}</li>
           ))}</ul>
-          <h4>Yeast:</h4><ul><li>{props.beer.ingredients.yeast}</li></ul>
-
+          </div>
+          </div>
+          <h4>Yeast</h4><ul><li>{props.beer.ingredients.yeast}</li></ul>
+          {props.children}
         </div>
 
   )}
